06b8a71364dcd3cfdd2a49853e4232f0ff5d6d1b
CybearTron <vinutron23@gmail.com>
2024-02-16 22:47:23 +0530

    Tried Lerp but failed; made a label work.

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,13 +1,28 @@
 extends CharacterBody2D
 
-@export var speed = 50000;
+@export var speed : float = 600;
+
+func _accelerate():
+	#speed+=0.1;
+	pass;
+
+func _decelerate():
+	#speed -=0.1;
+	pass;
 
 func _get_input(delta):
-	velocity = Input.get_vector("ui_left", "ui_right","ui_up","ui_down");
-	velocity *= delta * speed;
-	
+	var input_direction = Input.get_vector("ui_left", "ui_right","ui_up","ui_down")
+	velocity = input_direction * speed;
+	if input_direction != Vector2.ZERO:
+		$TestLabel.text = "Input is on";
+		_accelerate();
+	else:
+		$TestLabel.text = "Input is off";
+		_decelerate();
 
 
 func _physics_process(delta):
 	_get_input(delta)
 	move_and_slide();
+	
+
