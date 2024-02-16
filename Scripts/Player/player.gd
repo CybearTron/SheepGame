64a0f187b9a1eb0bdb24d11185e0d7f3be6751e0
CybearTron <vinutron23@gmail.com>
2024-02-16 23:49:57 +0530

    Made acceleration, deceleration and a placeholder tilemap.

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,28 +1,24 @@
 extends CharacterBody2D
 
-@export var speed : float = 600;
-
-func _accelerate():
-	#speed+=0.1;
-	pass;
-
-func _decelerate():
-	#speed -=0.1;
-	pass;
+@export var speed = 600;
+var acceleration: = 10;
+var friction = 400;
 
 func _get_input(delta):
-	var input_direction = Input.get_vector("ui_left", "ui_right","ui_up","ui_down")
-	velocity = input_direction * speed;
+	var input_direction =Input.get_vector("ui_left", "ui_right","ui_up","ui_down")
+	
+	velocity = velocity.move_toward(speed * input_direction, acceleration);
+	
 	if input_direction != Vector2.ZERO:
 		$TestLabel.text = "Input is on";
-		_accelerate();
+		
 	else:
 		$TestLabel.text = "Input is off";
-		_decelerate();
-
+	
 
 func _physics_process(delta):
 	_get_input(delta)
 	move_and_slide();
+	
 	
 
