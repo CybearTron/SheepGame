a8e1ceac70d0f2c0b4eee72b9349b529f2b5424a
CybearTron <vinutron23@gmail.com>
2024-02-16 18:51:29 +0530

    Added 8-way movement and tried to fix jitter, could not.

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,1 +1,13 @@
 extends CharacterBody2D
+
+@export var speed = 50000;
+
+func _get_input(delta):
+	velocity = Input.get_vector("ui_left", "ui_right","ui_up","ui_down");
+	velocity *= delta * speed;
+	
+
+
+func _physics_process(delta):
+	_get_input(delta)
+	move_and_slide();
